78a44d38f2422f1af7480f920cd448604818741a
Paulo Siroy <pausiroy@gmail.com>
2022-11-06 03:11:24 -0800

    Add local storage for name Input field

---
+++ main.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoForm = document.querySelector('#new-todo-form');
+
+    const username = localStorage.getItem('username') || "";
+
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
+
+
+})
+
